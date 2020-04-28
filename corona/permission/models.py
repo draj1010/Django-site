f259ce1eff8863137ce54687c2b7017db2b14fe5
draj1010 <45226595+draj1010@users.noreply.github.com>
2020-04-28 14:52:54 +0530

    Update models.py

--- corona/permission/models.py
+++ corona/permission/models.py
@@ -5,3 +5,9 @@ from django.db import models
 class User(AbstractUser):
     is_police = models.BooleanField(default=False)
     is_public = models.BooleanField(default=False)
+
+class Police(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+
+    def __str__(self):
+        return self.user.username
