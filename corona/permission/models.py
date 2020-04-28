6c3562f8721d84090a02ca8cb876a83cf3b054f2
draj1010 <45226595+draj1010@users.noreply.github.com>
2020-04-28 11:41:13 +0530

    superuser

--- corona/permission/models.py
+++ corona/permission/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+# # Create your models here.
+# class User(AbstractUser):
+#     is_police = models.BooleanField(default=False)
+#     is_public = models.BooleanField(default=False)
+
+# class People(models.Model):
